2713c64f9a3a38ac4a3901f552342cda02989674
Stanton Viaduc <sjviaduc@gmail.com>
2019-07-31 16:17:49 -0600

    small app that demonstrates two way binding

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import UserOutput from './UserOutput/UserOutput';
+import UserInput from './UserInput/UserInput';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    username: "Super Stan"
+  };
+
+  usernameChangedHandler = (event) => {
+    this.setState({username: event.target.value})
+  };
+  render(){
+    return (
+      <div className="App">
+        <UserInput
+            changed={this.usernameChangedHandler}
+            currentName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName={this.state.username}/>
+        <UserOutput userName="Stan"/>
+      </div>
+    );
+  }
 }
 
 export default App;
